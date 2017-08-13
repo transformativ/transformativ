86b915e8d09940ed4dc044a57670b0c5d4817c3e
Vaibhav Rajan <rajan.vaibhav@gmail.com>
2017-08-13 13:24:57 -0400

    v1.7

--- js/app.js
+++ js/app.js
@@ -3,57 +3,52 @@ $(document).foundation()
 var request;
 
 // Bind to the submit event of our form
-$("#foo").submit(function(event){
+$("#foo").submit(function(event) {
 
 
-    // Abort any pending request
-    if (request) {
-        request.abort();
+  // Abort any pending request
+  if (request) {
+    request.abort();
+  }
+  // setup some local variables
+  var $form = $(this);
+
+  // Let's select and cache all the fields
+  var $inputs = $form.find("input, select, button, textarea");
+
+  // Serialize the data in the form
+  var serializedData = $form.serialize();
+
+  // Let's disable the inputs for the duration of the Ajax request.
+  // Note: we disable elements AFTER the form data has been serialized.
+  // Disabled form elements will not be serialized.
+  $inputs.prop("disabled", true);
+
+  // Fire off the request to /form.php
+  request = $.ajax({
+    url: "https://script.google.com/macros/s/AKfycbyrf9xFPk_8jNak-9M1-Tyf0rFrA_yTJxTFSO4hfD-9o2wNsK8/exec",
+    type: "post",
+    data: serializedData,
+    success: function() {
+      document.getElementById('message').innerHTML = "Thank you for your message! We will get back to you shortly." + "<br><br><a href='./index.html'> Home </a>";
+      document.getElementById('smallmessage').innerHTML = "Thank you for your message! We will get back to you shortly." + "<br><br><div class='align_center'><a href='./index.html'> Home </a></div><br><br>";
     }
-    // setup some local variables
-    var $form = $(this);
-
-    // Let's select and cache all the fields
-    var $inputs = $form.find("input, select, button, textarea");
-
-    // Serialize the data in the form
-    var serializedData = $form.serialize();
-
-    // Let's disable the inputs for the duration of the Ajax request.
-    // Note: we disable elements AFTER the form data has been serialized.
-    // Disabled form elements will not be serialized.
-    $inputs.prop("disabled", true);
-
-    // Fire off the request to /form.php
-    request = $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbyrf9xFPk_8jNak-9M1-Tyf0rFrA_yTJxTFSO4hfD-9o2wNsK8/exec",
-        type: "post",
-        data: serializedData
-    });
+  });
 
 
-    // Callback handler that will be called on success
-    request.done(function (response, textStatus, jqXHR){
-        // Log a message to the console
-        alert ("coming here");
-        document.getElementById('message').innerHTML = "Thank you for your message! We will get back to you shortly." + "<br><br><a href='./index.html'> Home </a>";
-        document.getElementById('smallmessage').innerHTML = "Thank you for your message! We will get back to you shortly." + "<br><br><div class='align_center'><a href='./index.html'> Home </a></div><br><br>";
-
-    });
-
-    // Callback handler that will be called on failure
-    request.fail(function (jqXHR, textStatus, errorThrown){
-        // Log the error to the console
-        console.error(
-            "The following error occurred: "+
-            textStatus, errorThrown
-        );
-    });
+  // Callback handler that will be called on failure
+  request.fail(function(jqXHR, textStatus, errorThrown) {
+    // Log the error to the console
+    console.error(
+      "The following error occurred: " +
+      textStatus, errorThrown
+    );
+  });
 
 
 
-    // Prevent default posting of form
-    event.preventDefault();
+  // Prevent default posting of form
+  event.preventDefault();
 
 });
 
@@ -64,29 +59,27 @@ var check = function() {
 
 
 
-       if (document.getElementById('contactname').value == "") {
+  if (document.getElementById('contactname').value == "") {
 
-         document.getElementById('contactname').style.borderColor = "red";
-         err = 1;
-       } else {
-         document.getElementById('contactname').style.borderColor = "lightgray";
-       }
+    document.getElementById('contactname').style.borderColor = "red";
+    err = 1;
+  } else {
+    document.getElementById('contactname').style.borderColor = "lightgray";
+  }
 
-       if (document.getElementById('email').value == "") {
+  if (document.getElementById('email').value == "") {
 
-         document.getElementById('email').style.borderColor = "red";
-         err = 1;
-       } else {
-         document.getElementById('email').style.borderColor = "lightgray";
-       }
+    document.getElementById('email').style.borderColor = "red";
+    err = 1;
+  } else {
+    document.getElementById('email').style.borderColor = "lightgray";
+  }
 
-      if (err == 0) {
-        $("#foo").submit();
-      }
-
-      else {
-        document.getElementById('message').innerHTML = "Please fill-in the required fields";
-        document.getElementById('smallmessage').innerHTML = "Please fill-in the required fields";
-      }
+  if (err == 0) {
+    $("#foo").submit();
+  } else {
+    document.getElementById('message').innerHTML = "Please fill-in the required fields";
+    document.getElementById('smallmessage').innerHTML = "Please fill-in the required fields";
+  }
 
 }
